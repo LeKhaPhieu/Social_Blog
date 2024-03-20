934b88bae31f42166ea4ce47ffc8ab5cf80113d1
Le Kha Phieu <phieulk@regit-technology.com>
2024-03-20 17:36:59 +0700

    fix home page 1

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,24 @@
+const btnShowSidebar = document.getElementById('btnShowSidebar')
+const boxSidebarMobile = document.getElementById('sidebarMobile')
+const closeBoxSidebar = document.getElementById('closeSidebar')
+const overlay = document.getElementById('overlay')
 
+btnShowSidebar.addEventListener('click', function () {
+    boxSidebarMobile.style.display = 'block'
+})
+
+showSidebar = () => {
+
+    btnShowSidebar.onclick = () => {
+        boxSidebarMobile.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+}
+
+hiddenSidebar = () => {
+
+    closeBoxSidebar.onclick = () => {
+        boxSidebarMobile.style.display = 'none';
+        overlay.style.display = 'none';
+    }
+}
